7094522dba96f9047c57abad27251fe92fa5dadf
Gede Sutarsa <gede.sutarsa@gmail.com>
2018-12-30 11:20:37 +0800

    sync sblm pindah ke windows platform

--- core-client-commons/component/BaseComponent.tsx
+++ core-client-commons/component/BaseComponent.tsx
@@ -72,7 +72,10 @@ export abstract class BaseComponent<PROPS extends BaseComponentProps , STATE ext
                     destVal = readNested(comparedStateOrProp , f) ; 
                 }
                 if ( Array.isArray(srcVal) || Array.isArray(destVal)) {
-                    return !ObjectUtils.compareFieldsArray( srcVal , destVal , []) ; 
+                    let rtvl: boolean =  !ObjectUtils.compareFieldsArray( srcVal , destVal , []) ; 
+                    if ( rtvl) {
+                        return true ; 
+                    }
                 }
                 if ( srcVal !== destVal) {
                     return true ; 
